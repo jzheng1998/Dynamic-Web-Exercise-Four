8e91b89789f7527f63259b2d0c76bdaf1e150535
Jack Zheng <jackzheng784@gmail.com>
2020-11-14 19:22:53 -0500

    Deploy to heroku

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require("express");
-
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
+// Routes Import
 const indexRoute = require("./routes/index.js");
-const aboutRoute = require("./routes/about.js");
-
-// Server Static Files
-app.use("/static", express.static("public"));
 
 // Routing in Express
 app.use("/", indexRoute);
-app.use("/about", aboutRoute);
 
 app.listen(port, () => {
-  console.log("Exercise Three is running!");
+  console.log("Exercise Four is running!");
 });
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Routed Exercise Three");
-});
+router.get("/", (req, res) => res.send("Exercise Four"));
 
 module.exports = router;
