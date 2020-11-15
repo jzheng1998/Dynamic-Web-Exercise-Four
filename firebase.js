50dc092df2f0ad18caf2bbb41a9e94429dcf5cc3
Jack Zheng <jackzheng784@gmail.com>
2020-11-14 20:10:20 -0500

    Remove firebase config from gitignore

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxLeGOi9T4qIKDpVTkiIGiM8Xjn8mn2_U",
+  authDomain: "dm---exercise-four.firebaseapp.com",
+  databaseURL: "https://dm---exercise-four.firebaseio.com",
+  projectId: "dm---exercise-four",
+  storageBucket: "dm---exercise-four.appspot.com",
+  messagingSenderId: "977913621456",
+  appId: "1:977913621456:web:1f1ff441ac73cb03b527d5",
+};
+
+module.exports = firebaseConfig;
